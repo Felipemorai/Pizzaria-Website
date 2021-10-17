832946dca9869af36c8ab1a5f4eb63318f75b574
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-10-16 22:50:13 -0300

    Upgrade

---
+++ pizzaria-website/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I'll be the most rich, gorgeous, famous and loved guy in world.</h1>
+    </div>
+  );
+}
+
+export default App;
