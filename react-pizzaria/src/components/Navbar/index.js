5765622e2ed2283741999fcbe79589423aa6eac9
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-10-24 16:54:01 -0300

    Styled Components

---
+++ react-pizzaria/src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Nav } from './NavbarElements';
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                Nav
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
