4a550e8d5f2027eb5e00ac1e19791d304dfefd51
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-10-25 16:27:04 -0300

    NavIcon

--- react-pizzaria/src/components/Navbar/NavbarElements.js
+++ react-pizzaria/src/components/Navbar/NavbarElements.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { NavLink as Link} from 'react-router-dom';
+import { FaPizzaSlice } from 'react-icons/fa';
 
 export const Nav = styled.nav`
   background: red;
@@ -37,3 +38,8 @@ export const NavIcon = styled.div`
     font-weight: bold;
   }
 `;
+
+export const Bars = styled(FaPizzaSlice)`
+  font-size: 2rem;
+  transform: translate(-50%, -15%);
+`
