334b9633d8c5a14ef085484352611e08d8c16dbd
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-10-26 18:32:16 -0300

    Hero

--- react-pizzaria/src/App.js
+++ react-pizzaria/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero'
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Navbar />
+      <Hero />
     </Router>
   );
 }
---
+++ react-pizzaria/src/components/Hero/HeroElements.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const HeroContainer = styled.div`
+  background: linear-gradient(to right, rgba(0,0,0,0.7),
+  rgba(0,0,0,0.1)), url()(${ImgBg});
+`
