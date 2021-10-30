7886d4a0828b57e526d207cca18632af3e5950e5
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-10-30 07:56:04 -0300

    Pizzas

--- react-pizzaria/src/components/Products/data.js
+++ react-pizzaria/src/components/Products/data.js
@@ -4,6 +4,23 @@ export const productData = [
         alt: 'Pizza',
         name: 'Supreme Pizza',
         desc: 'Bacon, pepperoni slices, red and green bell pepper, red onion, black olives, mozzarella, parmesan and basil',
-        price: '$19.99'
+        price: '$19.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: 'Pizza',
+        name: 'Hawaiian Paradise',
+        desc: 'Combines pizza sauce, cheese, cooked ham, and pineapple',
+        price: '$16.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product3,
+        alt: 'Pizza',
+        name: 'Veggie Oveload',
+        desc: 'Cherry tomatoes, artichoke, bell pepper, olives, tomato sauce, mozzarell, red onion and spinach',
+        price: '$14.99',
+        button: 'Add to Cart'
     }
 ]
