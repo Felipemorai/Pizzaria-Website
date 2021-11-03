e3879c2ac93b816f41ed4d89b9b4c7b0a490a501
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-11-03 16:10:52 -0300

    FeaturePic

---
+++ react-pizzaria/src/components/Feature/FeatureElements.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import FeaturePic from '../../images/Pizza feature3.jpg'
+
+export const FeatureContainer = styled.div`
+  background: linear-grandient(to right, rgba(0, 0, 0,0.7), rgba(0, 0, 0, 0.1)),
+  url(${FeaturePic});
+  height: 100vh;
+  max-height: 500px;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  text-align: center;
+  padding: 0 1rem;
+
+  h1 {
+      font-size: clamp(3rem, 5vw, 5rem);
+  }
+`
--- react-pizzaria/src/components/Feature/index.js
+++ react-pizzaria/src/components/Feature/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { FeatureContainer, FeatureButton } from './FeatureElements';
 
 const Feature = () => {
     return (
