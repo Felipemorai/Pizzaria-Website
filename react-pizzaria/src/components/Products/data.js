073c5e5b9b6126cd9dbfed6d359aee442461cb47
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-11-04 15:52:51 -0300

    Upgrade

--- react-pizzaria/src/components/Products/data.js
+++ react-pizzaria/src/components/Products/data.js
@@ -1,6 +1,9 @@
 import product1 from '../../images/Supreme Pizza.jpg';
 import product2 from '../../images/Hawaiian Paradise.jpg';
 import product3 from '../../images/Veggie Overload.jpg';
+import doughnut from '../../images/Doughnuts.jpg';
+import caramel  from '../../images/Caramel-ice-cream.jpg';
+import brownie  from '../../images/Brownie.jpg';
 
 export const productData = [
     {
@@ -27,4 +30,31 @@ export const productData = [
         price: '$14.99',
         button: 'Add to Cart'
     }
-]
+];
+
+export const productDataTwo = [
+    {
+        img: doughnut,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Bacon, pepperoni slices, red and green bell pepper, red onion, black olives, mozzarella, parmesan and basil',
+        price: '$19.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: caramel,
+        alt: 'Pizza',
+        name: 'Hawaiian Paradise',
+        desc: 'Combines pizza sauce, cheese, cooked ham, and pineapple',
+        price: '$16.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: brownie,
+        alt: 'Pizza',
+        name: 'Veggie Oveload',
+        desc: 'Cherry tomatoes, artichoke, bell pepper, olives, tomato sauce, mozzarell, red onion and spinach',
+        price: '$14.99',
+        button: 'Add to Cart'
+    }
+];
